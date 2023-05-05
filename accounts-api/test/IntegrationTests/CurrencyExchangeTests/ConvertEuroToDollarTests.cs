dcd9bc3662222981f8a6529c67eb185d09b089a8
Chinan Yin <yinchinan168@gmail.com>
2023-05-05 14:27:46 +0800

    Initial commit

---
+++ accounts-api/test/IntegrationTests/CurrencyExchangeTests/ConvertEuroToDollarTests.cs
@@ -0,0 +1,30 @@
+﻿namespace IntegrationTests.CurrencyExchangeTests;
+
+using System.Threading.Tasks;
+using Domain.ValueObjects;
+using Infrastructure.CurrencyExchange;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+/// <summary>
+/// </summary>
+public sealed class ConvertEuroToDollarTests
+{
+    [Fact]
+    public async Task Convert()
+    {
+        ServiceCollection serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddHttpClient(CurrencyExchangeService.HttpClientName);
+        serviceCollection.AddSingleton<CurrencyExchangeFake>();
+
+        ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+        CurrencyExchangeFake sut = serviceProvider.GetRequiredService<CurrencyExchangeFake>();
+
+        Money usdMoney = new Money(100, Currency.Euro);
+        Money actual = await sut.Convert(usdMoney, Currency.Dollar);
+
+        Assert.True(actual.Amount > 100);
+        Assert.Equal("USD", actual.Currency.Code);
+    }
+}
